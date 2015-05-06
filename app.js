33f3abc0d4628861533cc0cee5eb93b4242db84e
L_x_C <lxc.neal@gmail.com>
2015-05-06 20:29:43 +0800

    officegen

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 var textract = require('textract');
-var fs = require('')
+var fs = require('fs');
+var officegen = require('officegen');
 
 var dic;
-fs.readFile('dic.json', 'utf-8', function(data) {
+fs.readFile('dic2.json', 'utf-8', function(err, data) {
 	dic = JSON.parse(data);
 });
 
-textract('a.doc', function(error, text) {
+textract('a.docx', 'utf-8',function(error, text) {
+	console.log(text)
 	for (var i in dic) {
 		var reg = new RegExp(i, 'g');
-		var replaceVal = dic[Math.floor(Math.random()*(dic[i].length - 1))];
+		var replaceVal = dic[i][Math.floor(Math.random() * (dic[i].length - 1))];
+		console.log(text);
 		text = text.replace(reg, replaceVal);
 	}
-	console.log(text);
+	var docx = officegen('docx');
+	docx.on('finalize', function(written) {
+		console.log('Finish to create Word file.\nTotal bytes created: ' + written + '\n');
+	});
+	docx.on('error', function(err) {
+		console.log(err);
+	});
+	var pObj = docx.createP();
+	pObj.addText(text);
+
+	var out = fs.createWriteStream('b.doc');
+
+	out.on('error', function(err) {
+		console.log(err);
+	});
+
+	docx.generate(out);
 });
