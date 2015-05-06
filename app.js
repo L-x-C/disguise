f7f4077a0760509721c0c8b17d52c914e63be3e9
L-x-C <lxc.neal@gmail.com>
2015-05-06 17:04:50 +0800

    dic

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var textract = require('textract');
-var dic = {
-	'啊': '去'
-}
+var fs = require('')
+
+var dic;
+fs.readFile('dic.json', 'utf-8', function(data) {
+	dic = JSON.parse(data);
+});
+
 textract('a.doc', function(error, text) {
 	for (var i in dic) {
 		var reg = new RegExp(i, 'g');
-		text = text.replace(reg, dic[i]);
+		var replaceVal = dic[Math.floor(Math.random()*(dic[i].length - 1))];
+		text = text.replace(reg, replaceVal);
 	}
 	console.log(text);
 });
--- crawler.js
+++ crawler.js
@@ -1,4 +1,6 @@
 var http = require('http');
+var cheerio = require('cheerio');
+var fs = require('fs');
 http.get('http://www.360doc.com/content/13/0617/10/380315_293418732.shtml', function(response) {
 	var stack = '';
 	response.on('data', function(chunk) {
@@ -10,7 +12,23 @@ http.get('http://www.360doc.com/content/13/0617/10/380315_293418732.shtml', func
 			console.log(err);
 		}
 		var $ = cheerio.load(stack);
-		
+		var $trArr = $('#articlecontent colgroup').next().find('tr');
+		var json = {};
+		$trArr.each(function(i, val) {
+			var tdArr = [];
+			$(val).find('td').each(function(i, ele) {
+				if ($(this).text()) {
+					tdArr.push($(this).text());
+				}
+			})
+			var valueArr = tdArr.slice(1);
+			json[tdArr[0]] = valueArr;
+		})
+		fs.appendFile('dic.json', JSON.stringify(json), function(err) {
+			if (err) {
+				console.log(err);
+			}
+		});
 	});
 }).on('error', function(err) {
 	callback();
